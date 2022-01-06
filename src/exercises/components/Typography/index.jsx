3c8bfa3220edbd286b81635b383346698a6cdad8
Hannah <hannahsenft@gmx.net>
2022-01-06 17:00:03 +0100

    more Typography

--- src/exercises/components/Typography/index.jsx
+++ src/exercises/components/Typography/index.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import styles from "./styles.module.css";
 
 const typeScale = {
-    h1: "h1",
-    h2: "h2",
-    h3: "h3",
-    caption: "small",
-    body1: "p",
-    body2: "p"
-}
+	h1: "h1",
+	h2: "h2",
+	h3: "h3",
+	caption: "small",
+	body1: "p",
+	body2: "p",
+};
 
-const Typography = ({ 
-    children, 
-    className,
-    variant="body1", 
-    component=typeScale[variant], 
-    ...rest }) => {
-	const Component = component ;
+const Typography = ({
+	children,
+	className,
+	variant = "body1",
+	component = typeScale[variant],
+	...rest
+}) => {
+	const Component = component;
 	return (
-		<Component {...rest} className={[styles[variant], className].join(" ")} >
+		<Component {...rest} className={[styles[variant], className].join(" ")}>
 			{children}
 		</Component>
 	);
